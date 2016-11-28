773f3db25f6e24b6a132c0a555dbc770088aa6dc
Ikhoon Eom <ih.pert@gmail.com>
2016-11-29 01:13:33 +0900

    Step 2: Integrating with HTML

---
+++ build.sbt
@@ -0,0 +1,7 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "Scala.js Tutorial"
+
+scalaVersion := "2.12.0"
+
+
---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,13 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+
+/**
+  * Created by ikhoon on 29/11/2016.
+  */
+object TutorialApp extends JSApp {
+
+  def main(): Unit = {
+    println("hello world")
+  }
+}
