d58f3573e2554afbe3f741388c1c1cd0b4e24c7d
Ikhoon Eom <ih.pert@gmail.com>
2016-11-29 01:41:54 +0900

    Step 5: Using jQuery

--- build.sbt
+++ build.sbt
@@ -4,7 +4,12 @@ name := "Scala.js Tutorial"
 
 scalaVersion := "2.12.0"
 
+skip in packageJSDependencies := false
+jsDependencies += "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.1"
+
 
 
 
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -5,6 +5,7 @@ import org.scalajs.dom.document
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
 
 /**
   * Created by ikhoon on 29/11/2016.
@@ -14,6 +15,7 @@ object TutorialApp extends JSApp {
   @JSExport
   def addClickedMessage(): Unit = {
     appendPar(document.body, "You clicked the button!")
+    appendParWithJQuery(document.body, "You clicked the button! with jquery")
   }
 
 
@@ -24,8 +26,17 @@ object TutorialApp extends JSApp {
     targetNode.appendChild(parNode)
   }
 
+  def appendParWithJQuery(targetNode: dom.Node, text: String): Unit = {
+    jQuery("body").append("<p>[message]</p>")
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello World</p>")
+  }
+
   def main(): Unit = {
-    appendPar(document.body, "Hello world")
+    jQuery(setupUI _)
   }
 }
 
